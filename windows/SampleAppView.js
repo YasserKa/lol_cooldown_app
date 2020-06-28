ef21b044167a6b57ba26e1de6dcd76b9c8c9e1c4
yasser kaddoura <yasser.kaddoura19@gmail.com>
2020-06-28 23:31:12 +0300

    Remvoing not needed code in main.html & sampleview

--- windows/SampleAppView.js
+++ windows/SampleAppView.js
@@ -2,14 +2,14 @@ define(["../scripts/services/drag-service.js"], function(DragService) {
   class SampleAppView {
     constructor() {
       // Methods:
-      this._showExitMinimizeModal = this._showExitMinimizeModal.bind(this);
-      this._hideExitMinimizeModal = this._hideExitMinimizeModal.bind(this);
+      // this._showExitMinimizeModal = this._showExitMinimizeModal.bind(this);
+      // this._hideExitMinimizeModal = this._hideExitMinimizeModal.bind(this);
       // Background window:
       this._backgroundWindow = overwolf.windows.getMainWindow();
       // Page elements:
-      this._modal = document.getElementById("exitMinimizeModal");
-      this._closeButton = document.getElementById("closeButton");
-      this._minimizeHeaderButton = document.getElementById("minimizeButton");
+      // this._modal = document.getElementById("exitMinimizeModal");
+      // this._closeButton = document.getElementById("closeButton");
+      // this._minimizeHeaderButton = document.getElementById("minimizeButton");
       this._exitButton = document.getElementById("exit");
       this._minimizeButton = document.getElementById("minimize");
       this._header = document.getElementsByClassName("app-header")[0];
@@ -20,11 +20,11 @@ define(["../scripts/services/drag-service.js"], function(DragService) {
 
     init() {
       // Listen to X button click
-      this._closeButton.addEventListener("click", this._showExitMinimizeModal);
+      // this._closeButton.addEventListener("click", this._showExitMinimizeModal);
       // Listen to minimize click
-      this._minimizeHeaderButton.addEventListener("click", () => {
-        this._backgroundWindow.minimize();
-      });
+      // this._minimizeHeaderButton.addEventListener("click", () => {
+      //   this._backgroundWindow.minimize();
+      // });
       // Close app on exit click
       this._exitButton.addEventListener("click", () => {
         this._backgroundWindow.close();
@@ -32,14 +32,14 @@ define(["../scripts/services/drag-service.js"], function(DragService) {
       // Minimize app on minimize click
       this._minimizeButton.addEventListener("click", () => {
         this._backgroundWindow.minimize();
-        this._hideExitMinimizeModal();
+        // this._hideExitMinimizeModal();
       });
       // When the user clicks anywhere outside of the modal, close it
-      window.onclick = function(event) {
-        if (event.target == this._modal) {
-          this._hideExitMinimizeModal();
-        }
-      }.bind(this);
+      // window.onclick = function(event) {
+      //   if (event.target == this._modal) {
+      //     this._hideExitMinimizeModal();
+      //   }
+      // }.bind(this);
       // Enable dragging on this window
       overwolf.windows.getCurrentWindow(result => {
         this.dragService = new DragService(result.window, this._header);
@@ -53,13 +53,13 @@ define(["../scripts/services/drag-service.js"], function(DragService) {
       });
     }
 
-    _showExitMinimizeModal() {
-      this._modal.style.display = "block";
-    }
+    // _showExitMinimizeModal() {
+    //   this._modal.style.display = "block";
+    // }
 
-    _hideExitMinimizeModal() {
-      this._modal.style.display = "none";
-    }
+    // _hideExitMinimizeModal() {
+    //   this._modal.style.display = "none";
+    // }
   }
 
   return SampleAppView;
