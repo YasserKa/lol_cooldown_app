c34ef15dc389396144d95c1038c3b5f33d169e55
yasser kaddoura <yasser.kaddoura19@gmail.com>
2020-06-29 00:24:59 +0300

    Refactoring parser.js - class extract (dataHandler.js)

---
+++ scripts/services/dataHandler.js
@@ -0,0 +1,106 @@
+define([],
+  function () {
+
+    let _initialized = false;
+
+    let _champions = {};
+    let _summonerSpells = {};
+    let _cdReductionTypes = {};
+    let _patchVersion = '';
+    let _runesReforged = {};
+    let _data = {};
+
+    function _init() {
+      if (_initialized)
+        return;
+
+      
+      _updateDataIfNeeded();
+
+      // _data = JSON.parse(localStorage.getItem("data"));
+
+      _champions = _data['champions'];
+      _summonerSpells = _data['spells'];
+      _cdReductionTypes = _data['cdReductionTypes'];
+      _runesReforged = _data['runes'];
+      _patchVersion = _data['version'];
+
+      _initialized = true;
+    }
+
+      // TODO:
+      // 1- Add time modified in server
+      // 2- Update if data modified is outdated
+      // 3- make a call just to check the version
+    async function _updateDataIfNeeded() {
+      _data = JSON.parse(localStorage.getItem("data"));
+      if (_data === null) {
+        let data = await _getDataFromServer();
+        localStorage.setItem("data", data);
+        _data = JSON.parse(data);
+      }
+    }
+
+    function _getDataFromServer() {
+      return new Promise(async (resolve, reject) => {
+        let path = 'https://www.lolcooldown.com/api/data';
+        let xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = () => {
+          if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 201) {
+              resolve(xhr.response);
+            } else {
+              reject({});
+            }
+          }
+        };
+        xhr.open("GET", path, true);
+        xhr.send();
+      });
+    }
+
+    function getChampionById(id) {
+      _init();
+      return _champions[Object.keys(_champions).find(key => _champions[key]['id'] === id)];
+    }
+
+    function getSpellById(id) {
+      _init();
+      return _summonerSpells[Object.keys(_summonerSpells).find(
+        key => parseInt(_summonerSpells[key]['key']) === id)];
+    }
+
+    function getCdReductionType(abilityName) {
+      _init();
+      // Loop through the list of abilities
+      for (let [type, abilities] of Object.entries(_cdReducitonTypes)) {
+        if (abilities.includes(abilityName)) {
+          return type;
+        }
+      }
+      return '';
+    }
+
+    function getCdDescription(champId, abilityKey) {
+      _init();
+      let effects = _champsDetails['abilities'][abilityKey][0]['effects'];
+      for (let effect of effects) {
+        if (effect['description'].includes('cooldown')) {
+          return effect['description'];
+        }
+      }
+    }
+    function getPatchVersion() {
+      _init();
+      return _patchVersion;
+    }
+
+    return {
+      getChampionById,
+      getSpellById,
+      getCdReductionType,
+      getCdDescription,
+      getPatchVersion,
+    }
+  });
--- scripts/services/parser.js
+++ scripts/services/parser.js
@@ -1,139 +1,9 @@
-/**
- * Detect whether champ select is currently running
- */
-define(["../../scripts/constants/states.js",],
-  function (States) {
-
-    let initialized = false;
-
-    let champions = {};
-    let summonerSpells = {};
-    let cdReducitonTypes = {};
-    let patchVersion = '';
-    let runesReforged = {};
-    let data = {};
-
-    function init() {
-      if (initialized)
-        return;
-
-      
-      data = JSON.parse(localStorage.getItem("data"));
-      // TODO:
-      // 1- Update if the version is outdated
-      // 2- make a call just to check the version
-      if (data === null) {
-        updateData();
-      }
-
-      champions = data['champions'];
-      summonerSpells = data['spells'];
-      cdReducitonTypes = data['cdReductionTypes'];
-      runesReforged = data['runes'];
-      patchVersion = data['version'];
-
-
-      initialized = true;
-    }
-
-    async function updateData() {
-      data = await getDataFromServer();
-      localStorage.setItem("data", data);
-      data = JSON.parse(data);
-    }
-
-    function getChampionById(id) {
-      return champions[Object.keys(champions).find(key => champions[key]['id'] === id)];
-    }
-
-    function getSpellById(id) {
-      return summonerSpells[Object.keys(summonerSpells).find(
-        key => parseInt(summonerSpells[key]['key']) === id)];
-    }
-
-    /** 
-     * data {
-     * 'assignedPosition',
-     * 'cellId',
-     * 'championId',
-     * 'championPickIntent',
-     * 'spell1Id',
-     * 'spell2Id',
-     * 'team',
-     * }
-     */
-    function getTeamData(team) {
-
-      let teamResult = [];
-      for (let participantData of team) {
-        // No champ has been picked yet
-        let champData = {};
-        // if (participantData['championId'] === 0) {
-        //   participantData['championId'] = 1;
-        // }
-        if (participantData['championId'] === 0) {
-          champData =  {
-            'abilities': [],
-            'name': 'no-champ',
-            'icon': '../../img/howling_abyss.png',
-          };
-        } else {
-          champData = getChampionInfo(participantData['championId']);
-          // Update the abilities by noting if abilities reduce the CD
-            for (let [key, ability] of Object.entries(champData['abilities'])) {
-
-              let abilityCdRedType = getCdReductionType(ability['name']);
-              champData['abilities'][key]['cooldownReduceType'] = abilityCdRedType;
-              // Getting the abilitiy's cooldown description
-              if (abilityCdRedType != '') {
-                let description = getCdDescription(participantData['championId'], key);
-                let array = description.split('cooldown');
-                let newDescription = array.join('<b>cooldown</b>');
-                champData['abilities'][key]['description'] = newDescription;
-              }
-            }
-          }
-
-          let spellsData = [
-            getSpellInfo(participantData['spell1Id']),
-            getSpellInfo(participantData['spell2Id'])
-          ];
-
-          // Prioritize flash
-          if (spellsData[1]['name'] == 'SummonerFlash')
-            [spellsData[0], spellsData[1]] = [spellsData[1], spellsData[0]];
-
-          let neededData = {
-            'cellId': participantData['cellId'],
-            'spells': spellsData,
-            'champion': champData,
-            // 'perks': participantData['cellId']
-          };
-
-          teamResult.push(neededData);
-        }
-        return teamResult;
-      }
-
-    function getCdReductionType(abilityName) {
-      // Loop through the list of abilities
-      for (let [type, abilities] of Object.entries(cdReducitonTypes)) {
-        if (abilities.includes(abilityName)) {
-          return type;
-        }
-      }
-      return '';
-    }
-
-    function getCdDescription(champId, abilityKey) {
-      let champsDetails = getChampionById(champId);
-      let effects = champsDetails['abilities'][abilityKey][0]['effects'];
-      for (let effect of effects) {
-        if (effect['description'].includes('cooldown')) {
-          return effect['description'];
-        }
-      }
-    }
+define([
+  "../../scripts/services/dataHandler.js",
+],
+  function (
+    dataHandler
+    ) {
 
     const EXCEPTION_DATA = {
       'Sett': {
@@ -183,11 +53,75 @@ define(["../../scripts/constants/states.js",],
       }
     };
 
+    /** 
+     * data {
+     * 'assignedPosition',
+     * 'cellId',
+     * 'championId',
+     * 'championPickIntent',
+     * 'spell1Id',
+     * 'spell2Id',
+     * 'team',
+     * }
+     */
+    function getTeamData(team) {
 
-    function getChampionInfo(id) {
-      init();
+      let teamResult = [];
+      for (let participantData of team) {
+        // No champ has been picked yet
+        let champData = {};
+        // if (participantData['championId'] === 0) {
+        //   participantData['championId'] = 1;
+        // }
+        if (participantData['championId'] === 0) {
+          champData =  {
+            'abilities': [],
+            'name': 'no-champ',
+            'icon': '../../img/howling_abyss.png',
+          };
+        } else {
+          champData = _getChampionInfo(participantData['championId']);
+          // Update the abilities by noting if abilities reduce the CD
+            for (let [key, ability] of Object.entries(champData['abilities'])) {
 
-      let champDetails = getChampionById(id);
+              let abilityCdRedType = dataHandler.getCdReductionType(ability['name']);
+              champData['abilities'][key]['cooldownReduceType'] = abilityCdRedType;
+              // Getting the abilitiy's cooldown description
+              if (abilityCdRedType != '') {
+                let description = dataHandler.getCdDescription(participantData['championId'], key);
+                let array = description.split('cooldown');
+                let newDescription = array.join('<b>cooldown</b>');
+                champData['abilities'][key]['description'] = newDescription;
+              }
+            }
+          }
+
+          let spellsData = [
+            _getSpellInfo(participantData['spell1Id']),
+            _getSpellInfo(participantData['spell2Id'])
+          ];
+
+          // TODO: shouldn't be the parser's job
+          // Prioritize flash
+          // if (spellsData[1]['name'] == 'SummonerFlash')
+          //   [spellsData[0], spellsData[1]] = [spellsData[1], spellsData[0]];
+
+          let neededData = {
+            'cellId': participantData['cellId'],
+            'spells': spellsData,
+            'champion': champData,
+            // 'perks': participantData['cellId']
+          };
+
+          teamResult.push(neededData);
+        }
+        return teamResult;
+      }
+
+    function _getChampionInfo(id) {
+
+      let patchVersion = dataHandler.getPatchVersion();
+      let champDetails = dataHandler.getChampionById(id);
       let abilities = champDetails['abilities'];
 
       data = {
@@ -222,7 +156,6 @@ define(["../../scripts/constants/states.js",],
         }
       };
 
-
       // TODO: remove later
       // Passive Cooldown: most 5 numbers is needed
       if (data['abilities']['P']['cooldowns'].length > 5) {
@@ -242,9 +175,9 @@ define(["../../scripts/constants/states.js",],
     }
 
 
-    function getSpellInfo(id) {
-      init();
-      let spell = getSpellById(id);
+    function _getSpellInfo(id) {
+      let spell = dataHandler.getSpellById(id);
+      let patchVersion = dataHandler.getPatchVersion();
       // Not known yet
       if (typeof(spell) === 'undefined') {
       return {
@@ -263,28 +196,7 @@ define(["../../scripts/constants/states.js",],
       };
     }
 
-    function getDataFromServer() {
-      return new Promise(async (resolve, reject) => {
-        let path = 'https://www.lolcooldown.com/api/data';
-        let xhr = new XMLHttpRequest();
-
-        xhr.onreadystatechange = () => {
-          if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 201) {
-              resolve(xhr.response);
-            } else {
-              reject({});
-            }
-          }
-        };
-        xhr.open("GET", path, true);
-        xhr.send();
-      });
-    }
     return {
-      updateData,
-      getChampionInfo,
       getTeamData,
-      getSpellInfo,
     }
   });
