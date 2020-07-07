4e5f62dfeaad50d604a2f12510368f32bfb14f68
yasser kaddoura <yasser.kaddoura19@gmail.com>
2020-07-08 00:19:55 +0300

    adding array_replace_recursive (js implementation) in parser.js

--- scripts/services/parser.js
+++ scripts/services/parser.js
@@ -323,7 +323,7 @@ define([
 
       // Deal with errors made by ddragon
       if (EXCEPTION_DATA.hasOwnProperty(champData['name'])) {
-        Object.assign(champData, EXCEPTION_DATA[champData['name']]);
+        champData = array_replace_recursive(champData, EXCEPTION_DATA[champData['name']]);
       }
 
       // Update the abilities by noting if abilities reduce the CD
@@ -362,6 +362,46 @@ define([
         'name': spell['id'],
         'description': spell['description'],
       };
+    }
+
+    // used similarly to the one in php
+    function array_replace_recursive(arr) {
+
+      var i = 0
+      var p = ''
+      var argl = arguments.length
+      var retObj
+
+      if (argl < 2) {
+        throw new Error('There should be at least 2 arguments passed to array_replace_recursive()')
+      }
+
+      // Although docs state that the arguments are passed in by reference,
+      // it seems they are not altered, but rather the copy that is returned
+      // So we make a copy here, instead of acting on arr itself
+      if (Object.prototype.toString.call(arr) === '[object Array]') {
+        retObj = []
+        for (p in arr) {
+          retObj.push(arr[p])
+        }
+      } else {
+        retObj = {}
+        for (p in arr) {
+          retObj[p] = arr[p]
+        }
+      }
+
+      for (i = 1; i < argl; i++) {
+        for (p in arguments[i]) {
+          if (retObj[p] && typeof retObj[p] === 'object') {
+            retObj[p] = array_replace_recursive(retObj[p], arguments[i][p])
+          } else {
+            retObj[p] = arguments[i][p]
+          }
+        }
+      }
+
+      return retObj
     }
 
     return {
