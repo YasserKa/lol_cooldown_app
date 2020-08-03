d051277318acea8f9827f2acd144ba58cf2d22eb
yasser kaddoura <yasser.kaddoura19@gmail.com>
2020-08-03 16:09:13 +0300

    updating the header title during champ-select & in-game transition

--- windows/app/app-view.js
+++ windows/app/app-view.js
@@ -1,37 +1,39 @@
 define([
   "../SampleAppView.js", 
 "../../scripts/services/htmlHandler.js",
+"../../scripts/services/hotkeys-service.js",
 "../../scripts/classes/Game.js",
 ],
   function (
     SampleAppView,
     HtmlHandler,
+    HotkeysService,
     Game) {
     class AppView extends SampleAppView {
 
-      _initialized = false;
-      _firstInGameUpdate = false;
-
       constructor() {
         super();
+        this._firstInGameUpdate = false;
+        this._firstChampSelectUpdate = false;
 
         this._update = this._update.bind(this);
         this.updateHotkey = this.updateHotkey.bind(this);
         this._hotkey = document.getElementById("hotkey");
+        this._headerTitle = document.getElementById("header-title");
         // message sent after a change in settings to update the view
         overwolf.windows.onMessageReceived.addListener((message)=>{
             HtmlHandler.update(this.game);
         });
       }
 
-      _init(data) {
-        this.game = new Game(data);
-        HtmlHandler.initializeView(this.game);
-
-        this._initialized = true;
-      }
-
       updateInChampSelect(data) {
+        if (!this._firstChampSelectUpdate) {
+          this._init(data);
+          this._firstChampSelectUpdate = true;
+          $(this._headerTitle).find('#game-state').text('Champ Select');
+          $(this._headerTitle).find('#hotkey-wrapper').css('display', 'none');
+        }
+
         this._update(data);
       }
 
@@ -39,20 +41,25 @@ define([
         if (!this._firstInGameUpdate) {
           this._init(data);
           this._firstInGameUpdate = true;
+          $(this._headerTitle).find('#game-state').text('In-Game');
+          $(this._headerTitle).find('#hotkey-wrapper').css('display', 'block');
         }
-        this._update(data);
-      }
 
-      _update(data) {
-        if (!this._initialized) {
-          this._init(data);
-        }
-        this.game.update(data);
-        HtmlHandler.update(this.game);
+        this._update(data);
       }
 
       updateHotkey(hotkey) {
         this._hotkey.textContent = hotkey;
+      }
+
+      _init(data) {
+        this.game = new Game(data);
+        HtmlHandler.initializeView(this.game);
+      }
+
+      _update(data) {
+        this.game.update(data);
+        HtmlHandler.update(this.game);
       }
     }
 
