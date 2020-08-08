b58dbf163ac82d323a26a8c900facf398e8a5144
yasser kaddoura <yasser.kaddoura19@gmail.com>
2020-08-08 20:04:34 +0300

    stopping main window from falshing in taskbar

--- windows/main/main-controller.js
+++ windows/main/main-controller.js
@@ -1,35 +1,38 @@
 define([
-  '../../windows/main/main-view.js',
-  "../../scripts/services/windows-service.js",
-  "../../scripts/constants/window-names.js",
+    '../../windows/main/main-view.js',
+    "../../scripts/services/windows-service.js",
+    "../../scripts/constants/window-names.js",
 ], function (
-  MainView,
-  WindowsService,
-  WindowNames,
-  ) {
+    MainView,
+    WindowsService,
+    WindowNames,
+) {
 
-  class MainController {
-    constructor() {
-      this.mainView = new MainView();
+    class MainController {
+        constructor() {
+            this.mainView = new MainView();
+        }
+
+        run() {
+            // disable window flashing in taskbar
+            overwolf.windows.flash(WindowNames.MAIN, overwolf.windows.enums.FlashBehavior.off);
+
+            overwolf.utils.getMonitorsList(async function (info) {
+                let display = info['displays'][0];
+                let height = display['height'];
+                let width = display['width'];
+                let window = await WindowsService.obtainWindow(WindowNames.MAIN);
+                let windowHeight = window['window']['height'];
+                let windowWidth = window['window']['width'];
+
+                let newTopPosition = parseInt((height-100)/2 - windowHeight/2);
+                let newLeftPosition = 0;
+
+                overwolf.windows.changePosition(WindowNames.MAIN, newLeftPosition, newTopPosition);
+            });
+        }
+
     }
 
-    run() {
-      overwolf.utils.getMonitorsList(async function (info) {
-        let display = info['displays'][0];
-        let height = display['height'];
-        let width = display['width'];
-        let window = await WindowsService.obtainWindow(WindowNames.MAIN);
-        let windowHeight = window['window']['height'];
-        let windowWidth = window['window']['width'];
-
-        let newTopPosition = parseInt((height-100)/2 - windowHeight/2);
-        let newLeftPosition = 0;
-
-        overwolf.windows.changePosition(WindowNames.MAIN, newLeftPosition, newTopPosition);
-      });
-    }
-
-  }
-
-  return MainController;
+    return MainController;
 });
