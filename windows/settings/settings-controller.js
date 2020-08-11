88c49c2b9a2262c4ee27083fbf19ed58d51b306b
yasser kaddoura <yasser.kaddoura19@gmail.com>
2020-08-11 12:48:05 +0300

    Refactor: settings-view/settings-controller

--- windows/settings/settings-controller.js
+++ windows/settings/settings-controller.js
@@ -1,9 +1,7 @@
 define([
     '../../windows/settings/settings-view.js',
-    "../../scripts/services/settings.js",
 ], function (
     SettingsView,
-    Settings,
 ) {
 
     class SettingsController {
@@ -12,26 +10,6 @@ define([
         }
 
         async run() {
-            // minute/second cooldown display
-            let cooldownDisplay = Settings.getSetting('cooldownDisplay');
-            var cooldownDisplayEl = $('input[value="' + cooldownDisplay + '"]');
-            cooldownDisplayEl.prop("checked", true);
-            cooldownDisplayEl.parent().addClass('active');
-            $('input[name="cooldown-display"]').click(function () {
-                let cooldownDisplay = $("input[name='cooldown-display']:checked").val();
-                Settings.setSetting('cooldownDisplay', cooldownDisplay)
-            });
-
-            // show hide cooldownreduction elements
-            let cooldownRedDisplay = Settings.getSetting('cooldownReductionDisplay') ? 'show' : 'hide';
-            var cooldownRedDisplayEl = $('input[value="' + cooldownRedDisplay + '"]');
-            cooldownRedDisplayEl.prop("checked", true);
-            cooldownRedDisplayEl.parent().addClass('active');
-            $('input[name="cooldown-reduction-display"]').click(function () {
-                let cooldownRedDisplay = $("input[name='cooldown-reduction-display']:checked").val() === 'show' ? true : false;
-                Settings.setSetting('cooldownReductionDisplay', cooldownRedDisplay)
-            });
-
         }
     }
 
--- windows/settings/settings-view.js
+++ windows/settings/settings-view.js
@@ -1,22 +1,54 @@
 define([
     '../base-view.js',
     '../../scripts/helpers/utils.js',
+    "../../scripts/services/settings.js",
 ], function (
     BaseView,
     Utils,
+    Settings,
 ) {
 
     class SettingsView extends BaseView {
         constructor() {
             super();
 
-
             this._version = document.getElementById("version");
 
-            this.updateVersion();
+            // minute/second cooldown display
+            this._initCooldownDisplaySetting();
+            // show hide cooldownreduction elements
+            this._initCooldownRedDisplay();
+
+            this._updateVersion();
         }
 
-        async updateVersion() {
+        _initCooldownDisplaySetting() {
+            let cooldownDisplay = Settings.getSetting('cooldownDisplay');
+            this._activateSettingEl(cooldownDisplay);
+            $('input[name="cooldown-display"]').click(function () {
+                let cooldownDisplay = $("input[name='cooldown-display']:checked").val();
+                Settings.setSetting('cooldownDisplay', cooldownDisplay)
+            });
+        }
+
+        _initCooldownRedDisplay() {
+            let cooldownRedDisplay = Settings.getSetting('cooldownReductionDisplay') ? 'show' : 'hide';
+            this._activateSettingEl(cooldownRedDisplay);
+            $('input[name="cooldown-reduction-display"]').click(function () {
+                let cooldownRedDisplay = $("input[name='cooldown-reduction-display']:checked").val() === 'show' ? true : false;
+                Settings.setSetting('cooldownReductionDisplay', cooldownRedDisplay)
+            });
+        }
+
+        _activateSettingEl(settingValue) {
+            let cooldownRedDisplayEl = $('input[value="' + settingValue + '"]');
+            cooldownRedDisplayEl.prop("checked", true);
+            cooldownRedDisplayEl.parent().addClass('active');
+        }
+
+
+
+        async _updateVersion() {
             const version = await Utils.getAppVersion();
             this._version.textContent = version;
         }
