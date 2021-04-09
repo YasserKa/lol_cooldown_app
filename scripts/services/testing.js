4d810923616e43db3910ce659f547edc6e3cf554
yasser kaddoura <yasser.kaddoura19@gmail.com>
2021-04-09 14:56:07 +0200

    update: add compressed files to gitignore

---
+++ scripts/services/testing.js
@@ -0,0 +1,246 @@
+define([
+  "../../scripts/constants/states.js",
+],
+  function (
+      States
+    ) {
+
+    const IS_TESTING = true;
+    // const STATE = States.IN_CHAMPSELECT;
+    const STATE = States.IN_GAME;
+    // const STATE = States.CHAMPSELECT_TO_GAME;
+    const FAKE_RUNES = false;
+
+    function isTesting() {
+        return IS_TESTING;
+    }
+
+    function isFakeRunes() {
+        return FAKE_RUNES;
+    }
+
+    function getState() {
+        return STATE;
+    }
+
+    function getInChampSelectData() {
+            let myTeam = [{
+                'championPicked': 81,
+                'cellId': 0,
+                'assignedPosition': '',
+                'spell1Id': 4,
+                'spell2Id': 12,
+                'team': 1,
+            },
+            {
+                'championPicked': 54,
+                'championId': 5,
+                'cellId': 1,
+                'assignedPosition': '',
+                'spell1Id': 4,
+                'spell2Id': 12,
+                'team': 1,
+            },
+            {
+                'championPicked': 10,
+                'cellId': 2,
+                'assignedPosition': '',
+                'spell1Id': 4,
+                'spell2Id': 12,
+                'team': 1,
+            },
+            {
+                'championPicked': 4,
+                'cellId': 3,
+                'assignedPosition': '',
+                'spell1Id': 4,
+                'spell2Id': 12,
+                'team': 2,
+            },
+            {
+                'championPicked': 1,
+                'cellId': 4,
+                'assignedPosition': '',
+                'spell1Id': 0,
+                'spell2Id': 0,
+                'team': 2,
+            },
+            {
+                'championPicked': 2,
+                'cellId': 5,
+                'assignedPosition': '',
+                'spell1Id': 4,
+                'spell2Id': 12,
+                'team': 2,
+            },
+        ];
+            let testData = {
+                'myTeam': myTeam,
+                'theirTeam': [],
+                'actions': [],
+            };
+            return testData;
+    }
+    function getInGameData() {
+            let players = [
+                {
+                    'summonerName': 'blue1',
+                    'championName': 'Ezreal',
+                    'position': '',
+                    'level': 1,
+                    'scores': {
+                        'creepScore': 10,
+                    },
+                    'summonerSpells': {
+                        'summonerSpellOne': {
+                            'displayName': 'Ignite',
+                        },
+                        'summonerSpellTwo': {
+                            'displayName': 'Flash',
+                        },
+                    },
+                    'items': [
+                        {'itemID': 3110},
+                        {'itemID': 3114}
+                    ],
+                    'team': 'ORDER',
+                }, {
+                    'summonerName': 'blue2',
+                    'championName': 'Ezreal',
+                    'position': '',
+                    'level': 1,
+                    'scores': {
+                        'creepScore': 10,
+                    },
+                    'summonerSpells': {
+                        'summonerSpellOne': {
+                            'displayName': 'Ignite',
+                        },
+                        'summonerSpellTwo': {
+                            'displayName': 'Flash',
+                        },
+                    },
+                    'items': [
+                        {'itemID': 3110},
+                        {'itemID': 3114}
+                    ],
+                    'team': 'ORDER',
+                }, {
+                    'summonerName': 'blue3',
+                    'championName': 'Ezreal',
+                    'position': '',
+                    'level': 1,
+                    'scores': {
+                        'creepScore': 10,
+                    },
+                    'summonerSpells': {
+                        'summonerSpellOne': {
+                            'displayName': 'Ignite',
+                        },
+                        'summonerSpellTwo': {
+                            'displayName': 'Flash',
+                        },
+                    },
+                    'items': [
+                        {'itemID': 3110},
+                        {'itemID': 3114}
+                    ],
+                    'team': 'ORDER',
+                }, {
+                    'summonerName': 'blue4',
+                    'championName': 'Ezreal',
+                    'position': '',
+                    'level': 1,
+                    'scores': {
+                        'creepScore': 10,
+                    },
+                    'summonerSpells': {
+                        'summonerSpellOne': {
+                            'displayName': 'Ignite',
+                        },
+                        'summonerSpellTwo': {
+                            'displayName': 'Flash',
+                        },
+                    },
+                    'items': [
+                        {'itemID': 3110},
+                        {'itemID': 3114}
+                    ],
+                    'team': 'ORDER',
+                }, {
+                    'summonerName': 'blue5',
+                    'championName': 'Ezreal',
+                    'position': '',
+                    'level': 1,
+                    'scores': {
+                        'creepScore': 10,
+                    },
+                    'summonerSpells': {
+                        'summonerSpellOne': {
+                            'displayName': 'Ignite',
+                        },
+                        'summonerSpellTwo': {
+                            'displayName': 'Flash',
+                        },
+                    },
+                    'items': [
+                        {'itemID': 3110},
+                        {'itemID': 3114}
+                    ],
+                    'team': 'ORDER',
+                }, {
+                    'summonerName': 'red1',
+                    'championName': 'Ezreal',
+                    'position': '',
+                    'level': 1,
+                    'scores': {
+                        'creepScore': 10,
+                    },
+                    'summonerSpells': {
+                        'summonerSpellOne': {
+                            'displayName': 'Ignite',
+                        },
+                        'summonerSpellTwo': {
+                            'displayName': 'Flash',
+                        },
+                    },
+                    'items': [
+                        {'itemID': 3110},
+                        {'itemID': 3114}
+                    ],
+                    'team': 'CHAOS',
+                },
+            ];
+            let runes = {
+                'Clumsy Gamer': {
+                perkIds: [5007, 8106, 8134, 8210, 8347],
+                perkStyle: 8000,
+                perkSubStyle: 8200,
+                },
+                'blue20': {
+                perkIds: [5007, 8106, 8134, 8210, 8347],
+                perkStyle: 8000,
+                perkSubStyle: 8200,
+                },
+                // 'red1': {
+                // perkIds: [5007, 8106, 8134, 8210, 8347],
+                // perkStyle: 8000,
+                // perkSubStyle: 8200,
+                // },
+            };
+
+            testData = {
+                'all_players': JSON.stringify(players),
+                'participantRunes': runes,
+            }
+        return testData;
+    }
+
+    return {
+      isTesting,
+      isFakeRunes,
+      getState,
+      getInChampSelectData,
+      getInGameData,
+    }
+  });
